49ffacb43be5881658f839d03809596c4017be8b
SenPy <65921678+18BD@users.noreply.github.com>
2022-01-02 03:45:31 +0600

    Update parol.py

--- parol.py
+++ parol.py
@@ -7,7 +7,7 @@ def parol(ff):
     pas = ''
     var = [c,b,b_2,s]
     if ff < 12:
-        return print('Ошибочка бро! пароль долден быть не менее 12 символов')
+        return 'Ошибочка бро! пароль должен иметь длину не менее 12 символов'
     else:
         pas += random.choice(c)
         pas += random.choice(b)
@@ -15,6 +15,6 @@ def parol(ff):
         pas += random.choice(s)
         while len(pas)<ff:
             pas += random.choice(var[random.randint(0,3)])
-        print(pas)
-parol(12)
+        return pas
+print(parol(12))
 
